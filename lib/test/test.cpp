bbab95fa1b0b790b148ac4c484f3a5f0dfeb1e0b
xunchangqing <xunchangqing@qq.com>
2016-04-02 09:05:20 +0800

    change model

--- lib/test/test.cpp
+++ lib/test/test.cpp
@@ -1,19 +1,23 @@
 #include <time.h>
 #include <stdio.h>
+#include <iostream>
 
 #include "opencv2/highgui/highgui.hpp"
 #include "object_detection.h"
 
 using namespace cv;
 int main() {
-  void *detector = detector_init(string("model/448_version_1.cfg").c_str(),
-                                 string("model/448_version_1.weights").c_str());
+  void *detector =
+      detector_init(string("../../../weights_models_ours/yolo_7c.cfg").c_str(),
+                    string("../../../weights_models_ours/yolo_7c_28000.weights").c_str());
   clock_t time;
   int objects[5 * MAX_OBJECTS] = {0};
   int obj_num;
-  VideoCapture vcap(0);
-  Mat src;
-  while (vcap.read(src)) {
+  //VideoCapture vcap(0);
+  //Mat src;
+  //while (vcap.read(src)) {
+  {
+    Mat src = imread("../../../screenshot/3.jpeg");
     //给定图像所存储的缓存，sdk负责检测
     //使用CV加载图像以后，需要进行处理，算法接受float类型的图像数据
     // Mat src = imread("test_images/person.jpg");
@@ -35,6 +39,7 @@ int main() {
         }
       }
     }
+    std::cout<<"after transform!"<<std::endl;
 
     time = clock();
     obj_num = detector_process_buffer(detector, img_buf, w, h, objects);
@@ -49,9 +54,11 @@ int main() {
                 2);
       objs += 5;
     }
+    delete[] img_buf;
     imshow("src", src);
-    if (waitKey(30) > 0)
-      break;
+    waitKey(0);
+    //if (waitKey(30) > 0)
+      //break;
   }
 
   /*cvWaitKey(0);*/
