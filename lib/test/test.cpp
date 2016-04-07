b8a0bc3b89989fd9c9698727d75b82f391dabc87
xunchangqing <xunchangqing@qq.com>
2016-04-07 17:17:23 +0800

    test using boost to list all files in a dir

--- lib/test/test.cpp
+++ lib/test/test.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <sstream>
+#include "boost/filesystem.hpp"
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -14,17 +15,43 @@
 using namespace cv;
 using namespace std;
 using namespace srzn_object_detection;
+namespace fs = ::boost::filesystem;
+
+// return the filenames of all files that have the specified extension
+// in the specified directory and all subdirectories
+void get_all(const fs::path &root, const string &ext, vector<fs::path> &ret) {
+  if (!fs::exists(root) || !fs::is_directory(root))
+    return;
+
+  fs::recursive_directory_iterator it(root);
+  fs::recursive_directory_iterator endit;
+
+  while (it != endit) {
+    if (fs::is_regular_file(*it) && it->path().extension() == ext)
+      ret.push_back(it->path().filename());
+    ++it;
+  }
+}
+
 int main() {
-  ObjectDetector object_detector(
-      "../../../models_object_detection/7c.cfg",
-      "../../../models_object_detection/7c_28000.weights");
-  VideoCapture vcap("../../../haoyun_atm_videos/atm_env_1.avi");
+  // VideoCapture vcap("../../../haoyun_atm_videos/atm_env_1.avi");
+  vector<fs::path> jpgs;
+  get_all("../../../haoyun_atm_videos/haoyun_bank_zhaoqing/JPEGImages", ".jpg",
+          jpgs);
+  //cout << jpgs[0].string() << endl;
   Mat src;
   int i = 0;
   clock_t time;
-  while (vcap.read(src)) {
-    if (i++ % (25) != 0)
-      continue;
+  ObjectDetector object_detector(
+      "../../../models_object_detection/7c.cfg",
+      "../../../models_object_detection/7c_28000.weights");
+  // while (vcap.read(src)) {
+  // if (i++ % (25) != 0)
+  // continue;
+  for (int i = 0; i < jpgs.size(); ++i) {
+    cout << jpgs[i].string() << endl;
+    src = imread("../../../haoyun_atm_videos/haoyun_bank_zhaoqing/JPEGImages/"+
+        jpgs[i].string());
     // imwrite("atm_env_1/"+SSTR(i)+".jpg", src);
     // Mat src = imread("../../../screenshot/1.jpeg");
     time = clock();
@@ -41,7 +68,7 @@ int main() {
     }
     resize(src, src, Size(0, 0), 0.5f, 0.5f);
     imshow("src", src);
-    waitKey(1);
+    waitKey(0);
     // if (waitKey(30) > 0)
     // break;
   }
